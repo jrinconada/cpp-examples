458b4a3869a4f02a70b42ea71c05cf4f4c3db8b2
julioenred <julioperezdecastro@gmail.com>
2018-02-26 17:04:12 +0100

    Client and server code cleaned

--- comunicaciones/client.cpp
+++ comunicaciones/client.cpp
@@ -1,21 +1,22 @@
+#include <iostream>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h> // read
-#include <arpa/inet.h>
+#include <arpa/inet.h> // inet_pton
 #define PORT 8080
 
 int main() {
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    const char *message = "Hola, hago peticiones";
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Error: creación de socket fallida" << '\n';
         return -1;
     }
 
@@ -25,19 +26,18 @@ int main() {
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        std::cerr << "Error: IP inválida" << '\n';
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Error: fallo de conexión" << '\n';
         return -1;
     }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    send(sock , message , strlen(message) , 0 );
+    std::cout << "Mensaje enviado" << message << '\n';
     valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
+    std::cout << "Mensaje recibido: " << buffer << '\n';
 }
--- comunicaciones/server.cpp
+++ comunicaciones/server.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -12,46 +13,38 @@ int main() {
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
+    const char *message = "Hola, estoy aquí para servirte";
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Error: creación de socket fallida" << '\n';
+        return -1;
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cerr << "Error: puerto en uso" << '\n';
+        return -1;
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        std::cerr << "Error: bind fallido" << '\n';
+        return -1;
     }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
+
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Error: listen fallido" << '\n';
+        return -1;
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        std::cerr << "Error: accept fallido" << '\n';
+        return -1;
     }
+
     valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    std::cout << "Mensaje recibido: " << buffer << '\n';
+    send(new_socket , message , strlen(message) , 0 );
+    std::cout << "Mensaje enviado: " << message << '\n';
 }
