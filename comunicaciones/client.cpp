c2ecdbf77eb0747423038ed26dc7a22e9ab24b51
Profe Juan <profejuan@mac.com>
2018-02-26 21:33:32 +0100

    Explanatory comments and more cleaning to server and client examples

--- comunicaciones/client.cpp
+++ comunicaciones/client.cpp
@@ -3,41 +3,76 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
-#include <string.h>
+#include <string.h> // strlen
 #include <unistd.h> // read
-#include <arpa/inet.h> // inet_pton
-#define PORT 8080
+#include <arpa/inet.h> // inet_aton
 
 int main() {
+    const int port = 8080;
+    const int bufferSize = 1024;
     struct sockaddr_in address;
     int sock = 0, valread;
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in serverAddress;
     const char *message = "Hola, hago peticiones";
-    char buffer[1024] = {0};
+    char buffer[bufferSize] = {0};
 
+    /*
+        socket: Creación de un socket
+            domain (AF_INET): Address Family IPv4
+            type (SOCK_STREAM): Socket TCP
+            protocol (0): TODO what is that???
+        Devuelve un número que representa el descriptor de fichero, -1 en caso de error
+    */
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         std::cerr << "Error: creación de socket fallida" << '\n';
         return -1;
     }
 
-    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(&serverAddress, '0', sizeof(serverAddress));
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    // TODO This and the next part is a little obscure
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+    /*
+        inet_aton: Transforma dirección IP de texto a binario
+            domain (AF_INET): Address Family IPv4
+            text ("127.0.0.1"): IP de la máquina actual (localhost)
+            serv_addr (serverAddress): IP y puerto del servidor
+        Devuelve un número mayor que cero si todo va bien
+    */
+    if(inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr)<=0) {
         std::cerr << "Error: IP inválida" << '\n';
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    /*
+        connect: Intenta conectar con un servidor
+            sockfd: Socket file descriptor
+            serv_addr: IP y puerto del servidor
+            addrlen: Tamaño de la estructura serv_addr
+        Devuelve -1 si hay un error
+    */
+    if (connect(sock, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
         std::cerr << "Error: fallo de conexión" << '\n';
         return -1;
     }
 
-    send(sock , message , strlen(message) , 0 );
+    /*
+        send: Envía un mensaje
+            sockfd: Socket file descriptor
+            message: Una cadena de texto
+            message_len: Tamaño del mensaje
+    */
+    send(sock , message , strlen(message) , 0);
     std::cout << "Mensaje enviado" << message << '\n';
-    valread = read( sock , buffer, 1024);
+
+    /*
+        read: Leer bytes del Socket
+        sockfd: Socket file descriptor
+        buffer: Almacenamiento de los bytes leídos
+        buffer_len: Tamaño del buffer
+    */
+    valread = read(sock , buffer, bufferSize);
     std::cout << "Mensaje recibido: " << buffer << '\n';
 }
--- comunicaciones/server.cpp
+++ comunicaciones/server.cpp
@@ -5,46 +5,67 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h> // read
-#define PORT 8080
 
 int main() {
-    int server_fd, new_socket, valread;
+    const int port = 8080;
+    int listeningSocket, commSocket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     const char *message = "Hola, estoy aquí para servirte";
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((listeningSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         std::cerr << "Error: creación de socket fallida" << '\n';
         return -1;
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         std::cerr << "Error: puerto en uso" << '\n';
         return -1;
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    /*
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    */
+    address.sin_family = AF_INET; // Address Family IPv4
+    address.sin_addr.s_addr = INADDR_ANY; // IP de esta máquina
+    address.sin_port = htons(port); // Puerto de Endian de Host a Endian de Network
+
+    /*
+        bind: Asignación de dirección IP y puerto
+            sockfd: socket file descriptor
+            serv_addr: Familia, IP y puerto del servidor
+            addrlen: Tamaño de la estructura que almacena la dirección
+        Devuelve -1 en caso de error
+    */
+    if (bind(listeningSocket, (struct sockaddr *)&address, sizeof(address))<0) {
         std::cerr << "Error: bind fallido" << '\n';
         return -1;
     }
 
-    if (listen(server_fd, 3) < 0) {
+    /*
+        listen: Se prepara para esperar una conexión en el socket indicado
+        sockfd: Socket file descriptor
+        backlog: Número de conexiones (clientes) en espera al mismo tiempo
+    */
+    if (listen(listeningSocket, 5) < 0) {
         std::cerr << "Error: listen fallido" << '\n';
         return -1;
     }
-    
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+
+    /*
+        accept: Espera una conexión
+        sockfd: Socket file descriptor
+        serv_addr: Dirección de donde se espera
+        addrlen: Tamaño de serv_addr
+    */
+    if ((commSocket = accept(listeningSocket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
         std::cerr << "Error: accept fallido" << '\n';
         return -1;
     }
 
-    valread = read( new_socket , buffer, 1024);
+    valread = read( commSocket , buffer, 1024);
     std::cout << "Mensaje recibido: " << buffer << '\n';
-    send(new_socket , message , strlen(message) , 0 );
+    send(commSocket , message , strlen(message) , 0 );
     std::cout << "Mensaje enviado: " << message << '\n';
 }
