d56c38e188d61f8f454efed2fcaebc7cd41e939b
Profe Juan <profejuan@criteria.com>
2018-03-02 10:48:29 +0100

    Minor reformatting and C++ I/O in process semaphore example

--- procesos/semaforo.cpp
+++ procesos/semaforo.cpp
@@ -1,30 +1,28 @@
-#include <stdio.h>
+#include <iostream> // cout
 #include <semaphore.h>
-#include <unistd.h>
+#include <unistd.h> // fork, getpid, sleep
 #include <sys/types.h> // wait
 #include <sys/wait.h> // wait
 #include <fcntl.h> // O_CREAT, O_EXCL
 
 // Compilación con -lpthread
 int main() {
-  // Error checking omitted for expository purposes
-  sem_t *sem = sem_open("test_semaphore", O_CREAT|O_EXCL, 0, 1);
-  sem_unlink("test_semaphore");
+  sem_t *sem = sem_open("semaphore", O_CREAT|O_EXCL, 0, 1);
+  sem_unlink("semaphore");
   int child = fork();
-  printf("pid %d about to wait\n", getpid());
+  std::cout << getpid() << " esperando..." << '\n';
   sem_wait(sem);
-  printf("pid %d done waiting\n", getpid());
+  std::cout << getpid() << " fin de espera" << '\n';
   sleep(1);
-  printf("pid %d done sleeping\n", getpid());
+  std::cout << getpid() << " durmiendo..." << '\n';
   sem_post(sem);
 
-  if(child > 0)
-  {
+  if(child > 0) {
     int status;
-    printf("parent done, waiting for child\n");
+    std::cout << "Esperando al hijo..." << '\n';
     wait(&status);
   }
 
-  printf("pid %d exiting\n", getpid());
+  std::cout << getpid() << " fin." << '\n';
   return 0;
 }
