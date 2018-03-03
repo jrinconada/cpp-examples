7021c44d1d5d437ca7d167449bb2566986a7fc59
Juan <juan@ubuntu.com>
2018-03-03 19:20:29 +0100

    stdlib added to use exit call in wait example

--- procesos/wait.cpp
+++ procesos/wait.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -12,7 +13,7 @@ int main () {
         std::cout << "Soy el hijo haciendo cosas..." << '\n';
         sleep(2); // Detener la ejecución 2 segundos
         std::cout << "Soy el hijo y he acabado." << '\n';
-        exit(42);
+        exit(42); // Termina y envía un código de retorno
     } else { // Código del padre
         std::cout << "Estoy esperando a mi hijo..." << '\n';
         pid_t child = wait(&status); // Wait es necesario para no crear procesos "zombie"
