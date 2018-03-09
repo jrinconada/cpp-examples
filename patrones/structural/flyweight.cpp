0cbdfe2b419e2e6dbf8403c36a91ba55b35119a9
Profe Juan <profejuan@criteria.com>
2018-03-09 12:07:25 +0100

    IDs mess in flywight pattern example fixed

--- patrones/structural/flyweight.cpp
+++ patrones/structural/flyweight.cpp
@@ -4,33 +4,35 @@
 using namespace std;
 
 class User {
-public:
     static long id;
+    long nameId;
+public:
     User(string name) {
+        id++;
         nameId = store(name);
     }
 
     string getName() {
         return names[nameId]; // devuelve el nombre para la clave almacenada
     }
-//private:
-    long nameId;
+
+    static long nameIdCount;
     static map<long, string> names;
     static map<string, long> ids;
 
     static long store (string name) {
         auto iterator = ids.find(name);
         if (iterator == ids.end()) { // Si no está en el mapa
-            names[id] = name; // Se inserta el nombre con el id actual
-            ids[name] = id; // Se inserta el id con el nombre actual
-            //id++;
-            return id++;
+            names[nameIdCount] = name; // Se inserta el nombre con el id actual
+            ids[name] = nameIdCount; // Se inserta el id con el nombre actual
+            return nameIdCount++;
         }
         return iterator->second; // Si está en el mapa se devuelve el id
     }
 };
 // Definiciones de variables estáticas
 long User::id = 0;
+long User::nameIdCount = 0;
 map<long, string> User::names;
 map<string, long> User::ids;
 
