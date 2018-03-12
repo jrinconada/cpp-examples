9a829f916f65e0cddf35b79a3100ac3d674ea3ff
Profe Juan <profejuan@mac.com>
2018-03-12 20:30:31 +0100

    Mutex example completed

--- hilos/mutex.cpp
+++ hilos/mutex.cpp
@@ -3,18 +3,46 @@
 #include <sstream>
 using namespace std;
 
-void countDown(string name) {
+mutex mtx;
+
+void print(string name, int i) {
     ostringstream text;
+    text << name << " : " << i << '\n';
+    cout << text.str();
+}
+
+void printMutex(string name, int i) {
+    mtx.lock();
+    cout << name << " : " << i << '\n';
+    mtx.unlock(); // Podría quedarse sin cerrar (por ejemplo por excepción o Ctrl-C)
+}
+
+void printMutexGuard(string name, int i) {
+    lock_guard<mutex> guard(mtx); // Previene la posibilidad de quedarse abierto
+    cout << name << " : " << i << '\n';
+}
+
+void countDown(string name) {
+
     for (int i = 30; i > 0; i--) {
-        text << name << " : " << i << '\n';
-        std::cout << text.str();
+        // Acceso a cout con condiciones de carrera
+        cout << name << " : " << i << '\n';
+        
+        // Acceso atómico a cout:
+        print(name, i);
+
+        // Acceso con mutex:
+        printMutex(name, i);
+
+        // Acceso con mutex guard:
+        printMutexGuard(name, i);
     }
 }
 
 int main() {
     thread t1(countDown, "HILO 1");
-    // thread t2(countDown, "HILO 2");
+    thread t2(countDown, "HILO 2");
 
     t1.join();
-    // t2.join();
+    t2.join();
 }
