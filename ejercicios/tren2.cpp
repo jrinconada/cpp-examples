fe4f70eb6a8119304faf600b0132d8f79eaba894
Profe Juan <profejuan@criteria.com>
2018-03-15 12:46:31 +0100

    Train excercise name formatted for clearer console representation

--- ejercicios/tren2.cpp
+++ ejercicios/tren2.cpp
@@ -3,26 +3,29 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include "../hilos/semaphore.cpp"
 using namespace std;
 
 struct Stop {
     string name;
-    mutex m;
+    Semaphore s;
     Stop(string name) : name(name) {}
 };
 
 struct Train {
     string name;
     vector<Stop*> stops;
-    
+
     Train(string name) : name(name) {}
 
     void go() {
         for (size_t i = 0; i < stops.size(); i++) {
             std::cout << name + " esperando " + stops[i]->name + '\n';
-            lock_guard<mutex> guard(stops[i]->m);
+            // lock_guard<mutex> guard(stops[i]->m);
+            stops[i]->s.wait();
             std::cout << name + " parado " + stops[i]->name + '\n';
             std::this_thread::sleep_for(std::chrono::seconds(2));
+            stops[i]->s.notify();
         }
     }
 };
@@ -35,8 +38,11 @@ int main() {
     Stop* s1 = new Stop("a");
     Stop* s2 = new Stop("b");
     Stop* s3 = new Stop("c");
+    s1->s.notify();
+    s2->s.notify();
+    s3->s.notify();
     Train* t1 = new Train("1");
-    Train* t2 = new Train("2");
+    Train* t2 = new Train("                2");
     t1->stops.push_back(s1);
     t1->stops.push_back(s2);
     t2->stops.push_back(s1);
